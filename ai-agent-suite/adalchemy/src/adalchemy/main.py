a77a27e529a6fa163c68931610d4263bbc7a07b0
Arjun Sarihyan <40883574+ArjunSarihyan@users.noreply.github.com>
2025-05-29 17:20:46 -0400

    Update main.py

--- ai-agent-suite/adalchemy/src/adalchemy/main.py
+++ ai-agent-suite/adalchemy/src/adalchemy/main.py
@@ -19,7 +19,7 @@ def run():
     """
     url = "https://www.huffpost.com/"
     inputs = {
-        'url': "str("URL of site")"
+        'url': "str(URL of site)"
         #'topic': 'URL Ad Review',
         #'current_year': str(datetime.now().year)
     }
